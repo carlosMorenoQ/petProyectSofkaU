498e6d045a1961f1d188c8f32092af0d60de312b
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-12-01 14:49:31 -0500

    finalizando funcionalidad de azar de pokemones

--- FrontEnd/pokeapp/src/components/Dashboard.jsx
+++ FrontEnd/pokeapp/src/components/Dashboard.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import Swal from 'sweetalert2'
 import OffCanvas from './OffCanvas'
 
 const StyledContainer = styled.div`
@@ -9,14 +11,14 @@ background-size: cover;
 background-attachment: fixed;
 height: 100%;
 //overflow: scroll;
-position: relative;
+
 
 .bienvenida{
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -60%);
-    background: #DCECED;
+    background: #C70039;
     border: solid 1px black;
     box-shadow: 4px 2px 2px black;
 }
@@ -30,7 +32,11 @@ a{
 }
 
 .card-img-top{
-    width: 130px;
+    width: 120px;
+    background: white;
+    padding: 20px;
+    border: solid 2px black;
+    border-radius: 50px;
 }
 
 @media (min-width: 600px) {
@@ -40,38 +46,86 @@ a{
 }
     
 `
+const API_URL = "https://pokeapi.co/api/v2/pokemon/"
+
 export const Dashboard = () => {
 
-    
+    const [pokemon, setPokemon] = useState([])
+    const [renew, setRenew] = useState(null)
+
+    useEffect(() => {
+
+        const random = Math.floor(Math.random() * (650 - 1) + 1);
+
+        axios.get(API_URL + random)
+            .then(res => {
+                setPokemon(res.data)
+            })
+            .catch(error => console.log(error))
+
+    }, [renew])
+
+    const handleAtrapar = () => {
+        const probabilidad = Math.floor(Math.random() * (100 - 1) + 1);
+
+        if (probabilidad >= 50) {
+            Swal.fire({
+                imageUrl: pokemon.sprites.other.dream_world.front_default,
+                imageWidth: 120,
+                background: '#CBE8B0',
+                backdrop: 'rgba(17, 17, 19, 0.973)',
+                title: `Atrapaste a ${pokemon.name.toUpperCase()}`,
+                showConfirmButton: false,
+                timer: 1500
+            })
+            setRenew(renew + 1)
+        } else {
+            Swal.fire({
+                imageUrl: 'https://i.pinimg.com/originals/a5/e0/05/a5e0050eef47f9fcad910e896b180a62.png',
+                imageWidth: 200,
+                background: '#E0AFA8',
+                backdrop: 'rgba(17, 17, 19, 0.973)',
+                title: `${pokemon.name.toUpperCase()} se ha escapado`,
+                showConfirmButton: false,
+                timer: 1500
+            })
+            setRenew(renew + 1)
+        }
+    }
+
+
     return (<StyledContainer>
 
         <div className="bienvenida rounded-3 d-flex p-3">
-            <div className="card-body">
-                <img
-                    src="https://cdn-icons-png.flaticon.com/512/188/188918.png"
-                    className="card-img-top"
-                    alt="pikachu" />
-                <hr />
-                <img
-                    src="https://fontmeme.com/permalink/211130/3a675333ef8d5f0f0659a4ffd2b2ba38.png"
-                    width={150}
-                    alt="fuente-pokemon"
-                    border="0" />
-                <p className="card-text text-center mt-1">¡Bienvenido a esta aventura!</p>
-                <div className="d-flex justify-content-center mt-4">
-                    <button
-                        
-                        className="btn btn-danger shadow-sm"
-                        // onClick={}
-                    >Tómalo</button>
-                    <button
-                        
-                        className="btn btn-outline-primary ms-1 shadow-sm"
-                        // onClick={}
-                    >Dejarlo pasar</button>
+            {pokemon.id === undefined ?
+                <h3>Cargando...</h3>
+                :
+                <div className="card-body">
+                    <img
+                        src={pokemon.sprites.other.dream_world.front_default}
+                        className="card-img-top"
+                        alt="pikachu" />
+                    <hr />
+                    <div className="d-flex justify-content-between mt-3">
+                        <h6 className="card-text text-center bg-warning p-1 rounded text-light">
+                            #{pokemon.id}
+                        </h6>
+                        <h6 className="card-text text-center text-light">
+                            {(pokemon.name).toUpperCase()}
+                        </h6>
+                    </div>
+                    <div className="d-flex justify-content-center mt-4">
+                        <button
+                            className="btn btn-success shadow-sm"
+                            onClick={handleAtrapar}
+                        >Atrapar</button>
+                        <button
+                            className="btn btn-outline-light ms-2 shadow-sm"
+                            onClick={() => setRenew(renew + 1)}
+                        >Dejar pasar</button>
+                    </div>
                 </div>
-
-            </div>
+            }
         </div>
 
 
